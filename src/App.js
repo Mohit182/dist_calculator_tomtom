87b0acbe06b85525a02e5cfb927a03dafbec296a
Mohit Gupta <mohitguptatech18@gmail.com>
2022-01-12 23:01:28 +0530

    tom tom api added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import * as tt from "@tomtom-international/web-sdk-maps";
 
 function App() {
+  const mapElement = useRef();
+  const [map, setMap] = useState({});
+  const [longitude, setLongitude] = useState(75.8108);
+  const [latitude, setLatitude] = useState(26.8640);
+
+  useEffect(() => {
+    let map = tt.map({
+      key: "1zYXDgLZZtd5NDEqhXnqNgNPEv3fErG4",
+      container: mapElement.current,
+      center: [longitude, latitude],
+      zoom: 15,
+    });
+
+    setMap(map);
+  }, []);
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div ref={mapElement} className="map"></div>
     </div>
   );
 }
